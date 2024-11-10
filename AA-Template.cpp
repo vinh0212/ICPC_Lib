4d149debb1da82d68bd0887a72e422d2c8356f54
vinh0212 <vinhqwert2004@gmail.com>
2024-11-10 11:32:30 +0700

    Add template

---
+++ AA-Template.cpp
@@ -0,0 +1,36 @@
+#define _USE_MATH_DEFINES
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fastio ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define fto(a,b,c) for(int a=b;a<=c;++a)
+#define fdto(a,b,c) for(int a=b;a>=c;--a)
+#define forit(it, var) for(__typeof(var.begin()) it = var.begin(); it != var.end(); ++it)
+#define ll long long
+#define db double
+#define ii pair<int,int>
+#define il pair<int,ll>
+#define pll pair<ll,ll>
+#define vi vector<int>
+#define vll vector<ll>
+#define vii vector<ii>
+#define INF (int)1e9
+#define LINF (ll)1e18
+#define ff first
+#define ss second
+#define mp(a,b) make_pair(a,b)
+#define sz size()
+#define pb push_back
+#define LSOne(S) ((S) & (-S))
+
+//#define int long long
+
+const int MAXN = 10005;
+const int MOD = 1e9 + 7;
+
+int main()
+{
+    fastio;
+    return 0;
+}
+    
