dd3109c52ada01f042dd3fae412c7aa8e03112d0
vinh0212 <vinhqwert2004@gmail.com>
2024-11-10 11:18:40 +0700

    Update rects misc

--- Misc/area of union of rectangles.cpp
+++ Misc/area of union of rectangles.cpp
@@ -1,89 +1,62 @@
-#include "../LazySegTree.h"
-
-// Area of union of rectangles {{{
-namespace area_of_union_of_rectangles {
-    using ll = long long;
-    const int INF = std::numeric_limits<int>::max() / 2;
-
-    struct Rect {
-        int x1, y1, x2, y2;
-    };
-    istream& operator >> (istream& cin, Rect& r) {
-        cin >> r.x1 >> r.y1 >> r.x2 >> r.y2;
-        return cin;
-    }
-
-    struct S {
-        int min_cnt;
-        ll sum;
-    };
-    S op(S x, S y) {
-        if (x.min_cnt < y.min_cnt) return x;
-        if (y.min_cnt < x.min_cnt) return y;
-        return { x.min_cnt, x.sum + y.sum };
-    }
-    S e() { return { INF, 0 }; }
-    S mapping(int f, S s) { return { s.min_cnt + f, s.sum }; }
-    int composition(int f, int g) { return f + g; }
-    int id() { return 0; }
-    using ST = LazySegTree<S, op, e, int, mapping, composition, id>;
-
-    ll solve(const std::vector<Rect>& rects) {
-        if (rects.empty()) return ll(0);
-
-        const int n = rects.size();
-        std::vector<std::tuple<int, int, int, int>> events; events.reserve(2*n);
-        std::vector<int> ys; ys.reserve(2*n);
-
-        for (const auto& r : rects) {
-            events.emplace_back(r.x1, r.y1, r.y2, +1);
-            events.emplace_back(r.x2, r.y1, r.y2, -1);
-            ys.push_back(r.y1);
-            ys.push_back(r.y2);
-        }
-        std::sort(events.begin(), events.end(), [] (const auto& e1, const auto& e2) {
-                return std::get<0>(e1) < std::get<0>(e2); });
-        std::sort(ys.begin(), ys.end());
-        ys.erase(std::unique(ys.begin(), ys.end()), ys.end());
-
-        const int nys = ys.size();
-        std::vector<S> init(nys - 1);
-        for (int i = 0; i < nys - 1; ++i) {
-            init[i] = { 0, ys[i+1] - ys[i] };
-        }
-        ST st(init);
-
-        ll res = 0;
-        ll lx = std::get<0>(events.front());  // events[i-1].x
-        for (int i = 0; lx != std::get<0>(events.back());) {
-            for (;; ++i) {
-                auto [xi, d, u, add] = events[i];
-                if (xi != lx) break;
-                int ly = std::lower_bound(ys.begin(), ys.end(), d) - ys.begin();
-                int ry = std::lower_bound(ys.begin(), ys.end(), u) - ys.begin();
-                st.apply(ly, ry, add);
-            }
-            ll rx = std::get<0> (events[i]);
-            auto [min_cnt, sum] = st.all_prod();
-            res += (rx - lx) * (ys.back() - ys.front() - (min_cnt == 0 ? sum : ll(0)));
-            lx = rx;
-        }
-        return res;
-    }
-}
-// }}}
-
-//Example Code
-#define PROBLEM "https://judge.yosupo.jp/problem/area_of_union_of_rectangles"
-
+//Tested: 
+// - https://codeforces.com/contest/610/problem/D
+// - https://oj.vnoi.info/problem/icpc23_mb_b
+// From CTP.LTT
 #include <bits/stdc++.h>
 using namespace std;
-#include "../misc/area_of_union_of_rectangles.h"
-using namespace area_of_union_of_rectangles;
+#define maxn 400005
+#define maxm 1000005
+
+int a[maxn], sum[maxn*4], cnt[maxn*4];
+
+void update(int l, int r, int low, int high, int id, int gt) {//range: [a[l], a[r])
+    if (high <= a[l] || a[r] <= low) return;
+    if (low <= a[l] && a[r] <= high) {
+        cnt[id] += gt;
+        if (cnt[id]) sum[id] = a[r]-a[l];
+        else sum[id] = sum[2*id] + sum[2*id+1];
+        return;
+    }
+    int mid = (l+r)/2;
+    update(l, mid, low, high, 2*id, gt);
+    update(mid, r, low, high, 2*id+1, gt);
+    if (!cnt[id]) sum[id] = sum[2*id] + sum[2*id+1];
+}
 
 int main() {
+    fastio;
     int n; cin >> n;
-    vector<Rect> rects(n);
-    for (auto& r : rects) cin >> r;
-    cout << solve(rects) << endl;
+    vector<int> tmp;
+    vector< pair<ii, ii> > sweepline; //sweepline: (x, type), (y1, y2)
+    fto (i, 1, n) {
+        int x1, x2, y1, y2; cin >> x1 >> y1 >> x2 >> y2;
+        if (x1 > x2) swap(x1, x2);
+        if (y1 > y2) swap(y1, y2);
+        ++y2; ++x2;
+        sweepline.pb(mp(mp(x1, 1), mp(y1, y2)));
+        sweepline.pb(mp(mp(x2, -1), mp(y1, y2)));
+        tmp.pb(y1); tmp.pb(y2);
+    }
+    int r = 0;
+    sort(tmp.begin(), tmp.end());
+    sort(sweepline.begin(), sweepline.end(), [&](pair<ii, ii> left, pair<ii, ii> right) {
+        return left.ff.ff < right.ff.ff;
+    });
+    fto (i, 0, (int)tmp.size()-1) {
+        a[++r] = tmp[i];
+        while (i+1 < tmp.size() && tmp[i] == tmp[i+1]) ++i;
+    }
+    long long ans = 0;
+    int pre = sweepline[0].ff.ff;
+    fto (i, 0, (int)sweepline.size()-1) {
+        ans = ans + (long long)(sweepline[i].ff.ff-pre)*sum[1];
+        pre = sweepline[i].ff.ff;
+        update(1, r, sweepline[i].ss.ff, sweepline[i].ss.ss, 1, sweepline[i].ff.ss);
+        while (i+1 < sweepline.size() && sweepline[i+1].ff.ff == sweepline[i].ff.ff) {
+            ++i;
+            update(1, r, sweepline[i].ss.ff, sweepline[i].ss.ss, 1, sweepline[i].ff.ss);
+        }
+    }
+    cout << ans << endl;
+    return 0;
 }
